a58fc135fab0a0d51213f8c963d0d17585dfd8dd
mattmcmurrer <mattmcmurrer@outlook.com>
2016-10-21 21:47:59 -0300

    Add files via upload

---
+++ Simple Calculator.py
@@ -0,0 +1,39 @@
+while True:
+    print()
+    print("Options:")
+    print("Enter 'add' to add two numbers")
+    print("Enter 'subtract' to subtract two numbers")
+    print("Enter 'multiply' to multiply two numbers")
+    print("Enter 'divide' to divide two numbers")
+    print("Enter 'quit' to end the program")
+    user_input = input(": ")
+
+    if user_input == "quit":
+        break
+
+    if user_input == "add":
+        num1 = float(input("Enter a number: "))
+        num2 = float(input("Enter another number: "))
+        result = str(num1 + num2)
+        print()
+        print("Answer: " + result)
+    elif user_input == "subtract":
+        num1 = float(input("Enter a number: "))
+        num2 = float(input("Enter another number: "))
+        result = str(num1 - num2)
+        print()
+        print("Answer: " + result)
+    elif user_input == "multiply":
+        num1 = float(input("Enter a number: "))
+        num2 = float(input("Enter another number: "))
+        result = str(num1 * num2)
+        print()
+        print("Answer: " + result)
+    elif user_input == "divide":
+        num1 = float(input("Enter a number: "))
+        num2 = float(input("Enter another number: "))
+        result = str(num1 / num2)
+        print()
+        print("Answer: " + result)
+    else:
+        print("Unknown Input")
